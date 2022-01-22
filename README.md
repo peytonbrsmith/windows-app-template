a7eaed18f329d847119f2d6edaed935cab75e30d
Peyton Smith <peytonbrsmith@gmail.com>
2022-01-21 23:39:41 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Windows app bootstrap template
+
+A simple template repository you can use to fast track your new modern Windows app. This template sets up a few common things you neeed for new projects:
+* MVVM repo structure
+* [MVVM toolkit pre-installed](https://devblogs.microsoft.com/pax-windows/mvvm-toolkit-preview-3-the-journey-of-an-api/)
+* [WinUI library pre-installed and pre-configured in app.xaml](https://github.com/Microsoft/microsoft-ui-xaml)
+* [AppCentre pre-installed for your telemetry needs](https://appcenter.ms)
+* [Translations already configured for Multilingual Toolkit](https://docs.microsoft.com/en-us/windows/uwp/design/globalizing/use-mat)
+* Example usage of RelayCommands included
+* Dependency injection already configured
+* Example usage of dependency injection
+
+This structure was also set up to be easily extended with [Xamarin](https://github.com/xamarin/Xamarin.Forms) or converted to [Uno](https://github.com/unoplatform/uno). 
+
+## Usage
+
+1. Click the "use this template" button
+2. This should generate a new repo that is pre-populated with the files in this template
+3. Clone your newly generated repo
+4. Use File Explorer or some other method to change the names of the projects, solutions, and namespaces as you see fit.
+5. Start building your app!
+
+## Contribution
+
+Ideas for new things to add are welcome, but I'd like to keep this repo as minimal as possible so apologies in advance if some ideas aren't added.
---
+++ src/MyApp.Uwp/App.xaml.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Diagnostics;
+using MyApp.Services;
+using MyApp.Services.Uwp;
+using MyApp.ViewModels;
+using MyApp.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+#nullable enable
+
+namespace MyApp
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        private IServiceProvider? _serviceProvider;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="IServiceProvider"/> instance for the current application instance.
+        /// </summary>
+        public static IServiceProvider Services
+        {
+            get
+            {
+                IServiceProvider? serviceProvider = ((App)Current)._serviceProvider;
+
+                if (serviceProvider is null)
+                {
+                    ThrowHelper.ThrowInvalidOperationException("The service provider is not initialized");
+                }
+
+                return serviceProvider;
+            }
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+                
+                // Setup dependency injection container
+                _serviceProvider = ConfigureServices();
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // Ensure the current window is active
+                Window.Current.Activate();
+            }
+        }
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+
+        /// <summary>
+        /// Configures a new <see cref="IServiceProvider"/> instance with the required services.
+        /// </summary>
+        private static IServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddTransient<ITelemetry, AppCentreTelemetry>()
+                .AddSingleton<MainPageViewModel>()
+                .BuildServiceProvider();
+        }
+    }
+}
